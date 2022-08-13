244d992ee8331192c0df0cc9c906c18723cdb703
Randy Kim <rhkim1292@gmail.com>
2022-08-13 01:47:42 -0700

    Implement a method that initializes game board
    The method is defined in the game board module since it's an operation
    that sets up the game board array.
    
    In order for the method to be able to search for the dom elements, the
    tic tac toe grid element was filled with 9 cell elements corresponding
    to each cell in the tic tac toe grid.

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const gameBoard = (() => {
-    'use strict';
+	"use strict";
+	const _gameBoardArrayLength = 9;
+	var _boardArray = [];
 
-    var _boardArray = [];
+	const initializeBoardArray = function () {
+		for (var i = 0; i < _gameBoardArrayLength; i++) {
+			_boardArray.push(document.querySelector(`#cell-${i}`));
+		}
+	};
+
+	const getBoardArray = () => {
+		return _boardArray;
+	};
+
+	return { getBoardArray, initializeBoardArray };
 })();
 
 const playerFactory = (name) => {
-    return {name};
+	return { name };
 };
 
+gameBoard.initializeBoardArray();
 const player1 = playerFactory("Randy");
 const player2 = playerFactory("Jimmy");
-
-console.log(player1);
-console.log(player2);
