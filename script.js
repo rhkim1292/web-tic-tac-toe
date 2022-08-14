1ef7ff421b57570b5272a9d817e3680e25574a85
Randy Kim <rhkim1292@gmail.com>
2022-08-14 00:46:43 -0700

    Make sure the cell the user clicked is not taken
    A function was created within the game logic module to check if the
    cell that the user clicked on is occupied by an X or an O.
    
    If this function returns true, turns will not be switched and a flag
    will be switched to true to let the display controller module know not
    to change anything in the display. An alert will also pop up to let the
    user know to choose a different cell.

--- script.js
+++ script.js
@@ -4,32 +4,48 @@ const playerFactory = (name, wins) => {
 
 const gameBoard = (() => {
 	"use strict";
-	const _gameBoardArrayLength = 9;
-	var _boardArray = [];
-    const gameBoardGridElement = document.querySelector(".tic-tac-toe-grid");
+	const _arrayLength = 9;
+	var _array = [];
+    const gridElement = document.querySelector(".tic-tac-toe-grid");
 
 	const initializeBoardArray = function () {
-		for (var i = 0; i < _gameBoardArrayLength; i++) {
-			_boardArray.push(document.querySelector(`#cell-${i}`));
+		for (var i = 0; i < _arrayLength; i++) {
+			_array.push(document.querySelector(`#cell-${i}`));
 		}
 	};
 
     const printBoardArray = function () {
-        console.log(_boardArray);
+        console.log(_array);
     };
 
-	return { initializeBoardArray, printBoardArray, gameBoardGridElement };
+	return { initializeBoardArray, printBoardArray, gridElement };
 })();
 
 const gameLogic = (() => {
     "use strict";
 
     var _p1Turn = true;
+    var _gameEnd = false;
+    var _lastActionErrored = false;
     const _player1 = playerFactory(prompt("Player 1, Enter your name"), 0);
     const _player2 = playerFactory(prompt("Player 2, Enter your name"), 0);
 
     const _switchPlayerTurn = function () {
         _p1Turn = !_p1Turn;
+    };
+
+    const _cellIsOccupied = function (selectedCell) {
+        if (selectedCell.className !== "cell-display") {
+            throw "Parameter is not a cell display element!";
+        }
+
+        if (selectedCell.textContent === "X" || selectedCell.textContent === "O") {
+            return true;
+        } else if (selectedCell.textContent === "") {
+            return false;
+        } else {
+            throw "Unrecognized text content within cell display!";
+        }
     };
 
     const isP1Turn = function () {
@@ -44,15 +60,26 @@ const gameLogic = (() => {
         return _player2.name;
     };
 
-    gameBoard.gameBoardGridElement.addEventListener("click", (e) => {
+    const lastActionErrored = function () {
+        return _lastActionErrored;
+    }
+
+    gameBoard.gridElement.addEventListener("click", (e) => {
         if (e.target.className !== "cell-display") {
             return;
         }
 
+        if (_cellIsOccupied(e.target)) {
+            _lastActionErrored = true;
+            alert("That cell is taken! Pick an empty cell!");
+            return;
+        }
+
         _switchPlayerTurn();
+        _lastActionErrored = false;
     });
 
-    return { isP1Turn, getP1Name, getP2Name };
+    return { isP1Turn, getP1Name, getP2Name, lastActionErrored };
 })();
 
 const displayController = (() => {
@@ -61,16 +88,16 @@ const displayController = (() => {
 
     _playerTurnDisplay.textContent = `${gameLogic.getP1Name()}'s Turn!`;
 
-    gameBoard.gameBoardGridElement.addEventListener("click", (e) => {
+    gameBoard.gridElement.addEventListener("click", (e) => {
         if (e.target.className !== "cell-display") {
             return;
         }
 
-        if (!gameLogic.isP1Turn()) {
+        if (!gameLogic.isP1Turn() && !gameLogic.lastActionErrored()) {
             // Display player 2 since  player 1 just clicked on a cell.
             _playerTurnDisplay.textContent = `${gameLogic.getP2Name()}'s Turn!`;
             e.target.textContent = "O";
-        } else {
+        } else if (gameLogic.isP1Turn() && !gameLogic.lastActionErrored()){
             // Display player 1 since player 2 just clicked on a cell.
             _playerTurnDisplay.textContent = `${gameLogic.getP1Name()}'s Turn!`;
             e.target.textContent = "X";
