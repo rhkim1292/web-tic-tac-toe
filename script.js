8c8607ced69a0b98b3c64ad33f14e6bd851e28c6
Randy Kim <rhkim1292@gmail.com>
2022-08-13 19:29:46 -0700

    Implement the game logic module
    So far, the game logic module alternates the player turns whenever a
    cell has been clicked on the tic tac toe grid.
    
    The display controller module was also modified to make sure O's are
    being displayed when it's player 1's turn and X's are being displayed
    when it's player 2's. The display controller module also lets the user
    know who's turn it is in a display above the tic tac toe grid.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const playerFactory = (name, wins) => {
+	return { name, wins };
+};
+
 const gameBoard = (() => {
 	"use strict";
 	const _gameBoardArrayLength = 9;
 	var _boardArray = [];
+    const gameBoardGridElement = document.querySelector(".tic-tac-toe-grid");
 
 	const initializeBoardArray = function () {
 		for (var i = 0; i < _gameBoardArrayLength; i++) {
@@ -13,30 +18,65 @@ const gameBoard = (() => {
         console.log(_boardArray);
     };
 
-	return { initializeBoardArray, printBoardArray };
+	return { initializeBoardArray, printBoardArray, gameBoardGridElement };
 })();
 
 const gameLogic = (() => {
     "use strict";
 
-})();
+    var _p1Turn = true;
+    const _player1 = playerFactory(prompt("Player 1, Enter your name"), 0);
+    const _player2 = playerFactory(prompt("Player 2, Enter your name"), 0);
 
-const displayController = (() => {
-    "use strict";
-    const _gameBoardGridElement = document.querySelector(".tic-tac-toe-grid");
+    const _switchPlayerTurn = function () {
+        _p1Turn = !_p1Turn;
+    };
 
-    _gameBoardGridElement.addEventListener("click", (e) => {
+    const isP1Turn = function () {
+        return _p1Turn;
+    };
+
+    const getP1Name = function () {
+        return _player1.name;
+    };
+
+    const getP2Name = function () {
+        return _player2.name;
+    };
+
+    gameBoard.gameBoardGridElement.addEventListener("click", (e) => {
         if (e.target.className !== "cell-display") {
             return;
         }
 
-        e.target.textContent = "X";
+        _switchPlayerTurn();
     });
+
+    return { isP1Turn, getP1Name, getP2Name };
 })();
 
-const playerFactory = (name) => {
-	return { name };
-};
+const displayController = (() => {
+    "use strict";
+    const _playerTurnDisplay = document.querySelector(".player-turn-display");
+
+    _playerTurnDisplay.textContent = `${gameLogic.getP1Name()}'s Turn!`;
+
+    gameBoard.gameBoardGridElement.addEventListener("click", (e) => {
+        if (e.target.className !== "cell-display") {
+            return;
+        }
+
+        if (!gameLogic.isP1Turn()) {
+            // Display player 2 since  player 1 just clicked on a cell.
+            _playerTurnDisplay.textContent = `${gameLogic.getP2Name()}'s Turn!`;
+            e.target.textContent = "O";
+        } else {
+            // Display player 1 since player 2 just clicked on a cell.
+            _playerTurnDisplay.textContent = `${gameLogic.getP1Name()}'s Turn!`;
+            e.target.textContent = "X";
+        }
+    });
+})();
 
 gameBoard.initializeBoardArray();
 gameBoard.printBoardArray();
